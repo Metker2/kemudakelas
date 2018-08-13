40bb5f24f1784f0971b3c99300c73ca1f6e3ffc5
Glitch (brick-tank) <none>
2018-08-13 18:08:40 +0000

    💐🐽 Checkpoint ./server.js:1615497/217 ./package.json:1615497/32

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const line = require('@line/bot-sdk');
 const express = require('express');
 const axios = require('axios');
+var randomWords = require('random-words');
+var ignoreCase = require('ignore-case');
 
 const config = {
   channelAccessToken: "3MMH93Gsk8BHnhk4ht4dtSsVp3UCbXBCI0KPcaDs0qDIoA/9BzvYC6cCde7oYNb3QFvOyru/5ImfWQF/iQ3FLbMsIX7ZoIcApM2fsk3l2pGPcZaoS31epFmXb8iOlpWc0qOXnGu/SrBv+8UmnHeiNgdB04t89/1O/w1cDnyilFU=",
@@ -23,10 +25,10 @@ app.post('/callback', line.middleware(config), (req, res) => {
 
 });
 
-function handleEvent(event) {
+function handleEvent(event){
   
-    if(event.message.text == "hai"){
-      const echo = { type: 'text', text: "Halo juga :)·" };
+    if(event.message.text == "apakah"){
+      const echo = { type: 'text', text: "skruutt :)" };
       return client.replyMessage(event.replyToken, echo);
     }
 
