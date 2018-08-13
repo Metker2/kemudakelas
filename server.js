d89740374532f031350df4149944ca3b11f72f65
Glitch (brick-tank) <none>
2018-08-13 17:48:26 +0000

    🌴🐀 Checkpoint ./server.js:1615497/1862 ./package.json:1615497/32

--- server.js
+++ server.js
@@ -1,22 +1,41 @@
-// server.js
-// where your node app starts
+const line = require('@line/bot-sdk');
+const express = require('express');
+const axios = require('axios');
 
-// init project
-var express = require('express');
-var app = express();
+const config = {
+  channelAccessToken: "3MMH93Gsk8BHnhk4ht4dtSsVp3UCbXBCI0KPcaDs0qDIoA/9BzvYC6cCde7oYNb3QFvOyru/5ImfWQF/iQ3FLbMsIX7ZoIcApM2fsk3l2pGPcZaoS31epFmXb8iOlpWc0qOXnGu/SrBv+8UmnHeiNgdB04t89/1O/w1cDnyilFU=",
+  channelSecret: "dc945c10ae01c8045a8db47e0cbd6bee",
+};
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+// create LINE SDK client
+const client = new line.Client(config);
+const app = express();
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((e)=>{
+      console.log(e);
+    });
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
 });
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+function handleEvent(event) {
+  
+    if(event.message.text == "hai"){
+      const echo = { type: 'text', text: "Halo juga :)·" };
+      return client.replyMessage(event.replyToken, echo);
+    }
+
+    const echo = { type: 'text', text: "Saya tidak mengerti, saya simpan dulu" };
+    return client.replyMessage(event.replyToken, echo);
+}
+
+// listen on port
+const port = 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
 });
